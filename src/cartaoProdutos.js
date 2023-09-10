8b069c6a58686222f2ec6dfb1d22bff5dc345d52
Leandro Vieira <si.leandrovieira@gmail.com>
2023-09-10 00:28:49 -0300

    v20230910 0027

---
+++ src/cartaoProdutos.js
@@ -0,0 +1,34 @@
+import { adicionarAoCarrinho } from "./menuCarrinho";
+import { catalogo } from "./utilidades";
+
+export function renderizarCatalogo(){
+    for(const produtoCatalogo of catalogo){
+        const cartaoProduto = 
+        `<div class="border-solid border-2 border-solid
+         w-48 m-2 flex flex-col p-2 justify-between shadow-xl shadow-slate-400 rounded-lg group" 
+        id="card-produto-${produtoCatalogo.id}">
+        <img 
+        src="./assets/img/${produtoCatalogo.imagem}" 
+        alt="Produto 1 do E-Commerce Levi." 
+        class='group-hover:scale-110 duration-300 my-3 rounded-lg'
+        />
+        <p>Id: ${produtoCatalogo.id}</p>
+        <p class = 'marca'>Marca: ${produtoCatalogo.marca}</p>
+        <p>Descrição: ${produtoCatalogo.nome}</p>
+        <p>Preço: $${produtoCatalogo.preco}</p>
+        <button id='adiciona - ${produtoCatalogo.id}' class='bg-slate-950 text-slate-200 hover:bg-slate-700'>
+            <i class="fa-solid fa-cart-plus"></i>
+        </button>
+        <p></p>
+        </div>`;
+
+        document.getElementById("container-produto").innerHTML 
+        += cartaoProduto;
+        document.getElementById(`adicionar-${produtoCatalogo.id}`);
+    }
+    for(const produtoCatalogo of catalogo){
+        document
+        .getElementById(`adicionar-${produtoCatalogo.id}`)
+        .addEventListener("click", adicionarAoCarrinho)
+    }
+}
--- src/menuCarrinho.js
+++ src/menuCarrinho.js
@@ -15,3 +15,23 @@ export function inicializarCarrinho(){
     botaoFecharCarrinho.addEventListener("click", fecharCarrinho);
     botaoAbrirCarrinho.addEventListener("click", abrirCarrinho);
 }
+
+export function adicionarAoCarrinho(){
+    const containerProdutosCarrinho = document.getElementById('produtos-carrinho');
+    const cartaoProdutoCarrinho = `
+    <article class="flex bg-slate-100 rounded-lg p-1 relative">
+    <button id="fechar-carrinho" class="absolute top-0 right-2">
+      <i class="fa-solid fa-circle-xmark text-slate-500 hover:text-slate-800"></i>
+    </button>
+    <img src="./assets/img/product-1.jpg" 
+      alt="Carrinho: Camisa Larga com Bolsos" 
+      class="h-24 rounded-lg"
+    />
+    <div class="py-2">
+      <p class="text-slate-900 text-sm">Camisa com Bolsos</p>
+      <p class="text-slate-400 text-xs">Tamanho: M</p>
+      <p class="text-green-700 text-lg">$70</p>
+    </div>
+  </article>`;
+  containerProdutosCarrinho.innerHTML += cartaoProdutoCarrinho;
+}
